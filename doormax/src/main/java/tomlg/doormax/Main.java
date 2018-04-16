23a64862025e0afd37963da6ce67bbab674d1a14
ferraz <matf.ferraz@gmail.com>
2018-04-16 16:01:56 -0300

    .

--- doormax/src/main/java/tomlg/doormax/Main.java
+++ doormax/src/main/java/tomlg/doormax/Main.java
@@ -8,7 +8,6 @@ import tomlg.doormax.utils.OOMDPReaderFromFile;
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		System.out.printf("Mi estas cxi tie");
 		OOMDPReaderFromFile a = new OOMDPReaderFromFile();
 		a.leitura();
 	}
