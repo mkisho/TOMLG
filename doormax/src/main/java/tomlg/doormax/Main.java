f3bec244fd94bc25f6e08706b49d5662c9d94ca7
ferraz <matf.ferraz@gmail.com>
2018-05-02 17:11:16 -0300

    Shimyureeshion no owari desu?

--- doormax/src/main/java/tomlg/doormax/Main.java
+++ doormax/src/main/java/tomlg/doormax/Main.java
@@ -16,7 +16,17 @@ public class Main {
 		OOMDP oomdp = a.leitura();
 		OOMDPState oomdpState = a.stateReader(oomdp);
 		TaxiEnvironment tmp = new TaxiEnvironment("Taxi");
-		Action a1 = new Action("pickupPassenger");
+		Action a1 = new Action("taxiMoveNorth");
+		tmp.simulateAction(oomdpState, a1);
+		a1 = new Action("taxiMoveWest");
+		tmp.simulateAction(oomdpState, a1);
+		a1 = new Action("taxiMoveEast");
+		tmp.simulateAction(oomdpState, a1);
+		a1 = new Action("taxiMoveSouth");
+		tmp.simulateAction(oomdpState, a1);
+		a1 = new Action("pickupPassenger");
+		tmp.simulateAction(oomdpState, a1);
+		a1 = new Action("dropOffPassenger");
 		tmp.simulateAction(oomdpState, a1);
 	}
 	
