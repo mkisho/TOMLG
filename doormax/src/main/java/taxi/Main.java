f1a7924cdba13427147605263d9a3e89f79904d8
simoes <c.lucas.simoes@gmail.com>
2018-06-09 11:49:36 -0300

    no errors

--- doormax/src/main/java/taxi/Main.java
+++ doormax/src/main/java/taxi/Main.java
@@ -4,10 +4,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-import burlap.behavior.singleagent.Policy;
-import burlap.oomdp.core.State;
-import burlap.oomdp.singleagent.GroundedAction;
 import taxi.pf.PassengerInTaxi;
 import taxi.pf.TaxiAtPassenger;
 import taxi.pf.WallToEastOfTaxi;
@@ -15,7 +11,10 @@ import taxi.pf.WallToNorthOfTaxi;
 import taxi.pf.WallToSouthOfTaxi;
 import taxi.pf.WallToWestOfTaxi;
 import tomlg.doormax.Doormax;
+import tomlg.doormax.Effect;
 import tomlg.doormax.PropositionalFunction;
+import tomlg.doormax.effects.EffectType;
+import tomlg.doormax.oomdpformalism.Action;
 import tomlg.doormax.oomdpformalism.OOMDP;
 import tomlg.doormax.oomdpformalism.OOMDPState;
 import tomlg.doormax.utils.OOMDPReaderFromFile;
@@ -32,42 +31,46 @@ public class Main {
 		pfs.add(new WallToNorthOfTaxi());
 		pfs.add(new WallToSouthOfTaxi());
 		pfs.add(new WallToWestOfTaxi());
-		
-		PropositionalFunction [] pfss = new PropositionalFunction[pfs.size()];
-		for (int i=0; i< pfs.size(); i++)
+
+		PropositionalFunction[] pfss = new PropositionalFunction[pfs.size()];
+		for (int i = 0; i < pfs.size(); i++)
 			pfss[i] = pfs.get(i);
-		
-		RandomGameGenerator rgg =new RandomGameGenerator("aa.txt");
-		while(!rgg.gerar());
-		
+		RandomGameGenerator rgg = new RandomGameGenerator("aa.txt");
+		rgg.gerar();
 		OOMDP oomdp = a.leitura(pfss);
 		OOMDPState initialState = a.stateReader(oomdp);
 		TaxiEnvironment evs = new TaxiEnvironment("Taxi");
-		Doormax doormax = new Doormax(oomdp, initialState, 100, 0.1);
-	
+		
+		List<EffectType> effectsToUse = new ArrayList<>();
+		for(EffectType e : Effect.γ) {
+			effectsToUse.add(e);
+		}
+		
+		Doormax doormax = new Doormax(oomdp, pfs, effectsToUse,initialState,
+				100, null);
+
 		OOMDPState curState = initialState;
 		int steps = 0;
-		while(!steps < 10){
-			
-			GroundedAction ga = (GroundedAction)policy.getAction(curState);
-			State nextState = ga.executeIn(curState);
-			double r = this.rf.reward(curState, ga, nextState);
-			
-			ea.recordTransitionTo(ga, nextState, r);
-			
-			if(!this.model.transitionIsModeled(curState, ga)){
-				this.model.updateModel(curState, ga, nextState, r, this.tf.isTerminal(nextState));
-				if(this.model.transitionIsModeled(curState, ga)){
-					this.modelPlanner.modelChanged(curState);
-					//policy = new DomainMappedPolicy(domain, this.modelPlanner.modelPlannedPolicy());
-					policy = this.createDomainMappedPolicy();
+		while (steps < 10) {
+
+			Action ga = oomdp.actions.get(0);
+			OOMDPState nextState = evs.simulateAction(curState, ga);
+			double r = -1;
+
+			if (!doormax.transitionIsModeled(curState, ga)) {
+				doormax.updateModel(curState, ga, nextState, r, false);
+				if (doormax.transitionIsModeled(curState, ga)) {
+					// doormax.modelPlanner.modelChanged(curState);
+					// policy = new DomainMappedPolicy(domain,
+					// this.modelPlanner.modelPlannedPolicy());
+					// policy = this.createDomainMappedPolicy();
 				}
 			}
 			
-			
 			curState = nextState;
-			
+
 			steps++;
+			break;
 		}
 	}
 }
